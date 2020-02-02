61388d74ed5cca39a852246a52e66f8e66d2d6bd
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-02 14:50:22 -0300

    Script - BD-SQL

--- Sql Server/LojaDeSuco.sql
+++ Sql Server/LojaDeSuco.sql
@@ -35,16 +35,26 @@ CREATE TABLE tb_Vendas(
 CREATE TABLE tb_ProdutoVenda(
 	cdProdutoVenda INT PRIMARY KEY IDENTITY(1,1),
 	cdVenda INT FOREIGN KEY REFERENCES tb_Vendas(cdVenda),
-	cdProduto INT FOREIGN KEY REFERENCES tb_DadosProduto(cdProduto)
+	cdProduto INT FOREIGN KEY REFERENCES tb_DadosProduto(cdProduto) NOT NULL,
+	qtProduto INT NOT NULL
 );
 
 --DML - LINGUAGEM DE MODELAGEM DE DADOS
 INSERT INTO tb_DadosProduto VALUES('Suco de Maracuja', 3.5),
 ('Suco de Goiaba', 8.5);
 
+INSERT INTo tb_Clientes VALUES('Paulo Guina', '1980-05-01', 'M','Rua da Mecanica, 400', 'Campinhas', 'São Paulo','11 2234-5654', '11 3243-3243');
+
+INSERT INTO tb_Vendas VALUES (1,GETDATE());
 --DML
 INSERT INTO tb_Vendas VALUES (5, 3.6);
 
+INSERT INTO tb_ProdutoVenda VALUES(1,1,3);
+INSERT INTO tb_ProdutoVenda VALUES(1,2,3);
+INSERT INTO tb_ProdutoVenda VALUES(8,3,4);
+
+
+--SOMA DOS VALORES DAS TABELAS
 
 DROP TABLE tb_Vendas;
 
@@ -59,6 +69,19 @@ REFERENCES tb_Produtos(cdProduto)
 SELECT * FROM tb_DadosProduto ;
 SELECT * FROM tb_Vendas;
 SELECT * FROM tb_Clientes;
+SELECT * FROM tb_ProdutoVenda;
 
 --SELECT [Colunas] FROM [tabela]
 SELECT nmProduto FROM tb_DadosProduto;
+
+/*SELECT V.cdVenda, C.nmCliente, P.cdProduto, P.nmProduto, PV.qtProduto, P.vlProduto, qtProduto * vlProduto AS ValorTotal
+FROM tb_DadosProduto P INNER JOIN tb_ProdutoVenda PV ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_Vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente*/
+
+
+SELECT V.cdVenda, C.nmCliente,NULL,'Total Produtos - Total da Vendas', SUM(PV.qtProduto) qtTotalProdutos, SUM(P.vlProduto * PV.qtProduto) AS qtValorTotalVenda --, P.vlProduto, qtProduto * vlProduto AS ValorTotal
+FROM tb_DadosProduto P INNER JOIN tb_ProdutoVenda PV ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_Vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente
+GROUP BY V.cdVenda, C.nmCliente

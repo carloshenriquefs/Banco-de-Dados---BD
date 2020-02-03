174450b45a05ce003e7cf8e94994e0aaf2d9c53e
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-02 23:50:20 -0300

    Livros - Script

--- Sql Server/LIVROS.sql
+++ Sql Server/LIVROS.sql
@@ -2,21 +2,25 @@
 CREATE DATABASE LIVROS;
 go
 
+--DDL
 USE LIVROS;
 go
 
+--DDL
 CREATE TABLE Generos(
 	IdGenero INT PRIMARY KEY IDENTITY,
 	NomeGenero VARCHAR(200) NOT NULL
 );
 go
 
+--DDL
 CREATE TABLE Autores(
 	IdAutor INT PRIMARY KEY IDENTITY,
 	NomeAutor VARCHAR(200) NOT NULL
 );
 go
 
+--DDL
 CREATE TABLE Livros(
 	IdLivro INT PRIMARY KEY IDENTITY,
 	Titulo VARCHAR(200) NOT NULL,
@@ -32,12 +36,14 @@ INSERT INTO Generos VALUES ('Romance'),
 						   ('Terror'),
 						   ('Administracao');
 
+--DML
 INSERT INTO Autores VALUES('Autor A'),
 						  ('Autor B'),
 						  ('Autor C'),
 						  ('Autor D'),
 						  ('Autor E');
 
+--DML
 INSERT INTO Livros VALUES ('Titulo A',1,1),
 						  ('Titulo B',2,2),
 						  ('Titulo C',3,3),
@@ -51,29 +57,35 @@ UPDATE Generos
 SET NomeGenero = 'Matematica'
 WHERE IdGenero = 1;
 
+--DML
 --Alterar o nome de um livro | TITULO A por A CABANA
 UPDATE Livros
 SET Titulo = 'A Cabana'
 WHERE IdLivro = 1;
 
+--DML
 --Apagando o Autor | Titulo B
 DELETE FROM Livros
 WHERE IdAutor = 2;
 
+--DML
 --Apagando o Autor | Autor B
 DELETE FROM Autores
 WHERE IdAutor = 2;
 
+--DQL
 --SELECIONAR TODOS OS LIVROS E SEUS RESPECTIVOS AUTORES
 SELECT L.Titulo, A.NomeAutor
 FROM Livros L INNER JOIN Autores A
 ON L.IdAutor = A.IdAutor
 
+--DQL
 --SELECIONAR TODOS OS LIVROS E SEUS RESPECTIVOS GÊNEROS
 SELECT L.Titulo, G.NomeGenero
 FROM Livros L INNER JOIN Generos G
 ON L.IdGenero = G.IdGenero;
 
+--DQL
 --SELECIONAR TODOS OS LIVROS E SEUS RESPECTIVOS AUTORES E GENEROS
 SELECT L.Titulo, A.NomeAutor,G.NomeGenero
 FROM Livros L INNER JOIN Autores A ON L.IdAutor = A.IdAutor

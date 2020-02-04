6925e99b20df02207c30abc8c926c6fd7d40206b
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-03 21:25:29 -0300

    Ecommerce-Exercicio

--- ExericiosSQL/Clinica1.3/PClinica-1.3-DQL.sql
+++ ExericiosSQL/Clinica1.3/PClinica-1.3-DQL.sql
@@ -6,10 +6,31 @@ USE PCLINICAmanha;
 
 */
 
-SELECT RazaoSocial,Endereco,NomeDono,Titulo,NomeVeterinario,CRMV,NomePet,Telefone,Titulo,Data,Descricao
+SELECT RazaoSocial,Endereco,NomeDono,NomeVeterinario,CRMV,NomePet,Telefone,Data,Descricao
 FROM Clinica C INNER JOIN  Veterinario V ON C.IdClinica = V.IdClinica
-INNER JOIN Atendimento C ON C.IdVet = C.IdVet
-INNER JOIN Pet P ON C.IdPet = P.IdPet
-INNER JOIN Dono D ON P.IdDono = D.IdDono
+INNER JOIN Atendimento A ON A.IdVet = V.IdVet
+INNER JOIN Pet P ON P.IdPet = A.IdPet
+INNER JOIN Dono D ON D.IdDono = P.IdDono
+INNER JOIN Raca R ON P.IdRaca = R.IdRaca
+INNER JOIN TipoPet TP ON R.IdTipoPet = TP.IdTipoPet
+
+
+SELECT RazaoSocial,Endereco,NomeDono,NomeVeterinario,CRMV,NomePet,Telefone,Data,Descricao
+FROM Clinica C INNER JOIN Veterinario V ON C.IdClinica = V.IdClinica
+INNER JOIN Atendimento A ON A.IdVet = V.IdVet
+INNER JOIN Pet P ON P.IdPet = A.IdPet
+INNER JOIN Dono D ON D.IdDono = P.IdDono
 INNER JOIN Raca R ON R.IdRaca = P.IdRaca
-INNER JOIN TipoPet TP ON R.IdRaca = TP.IdRaca
+INNER JOIN TipoPet TP ON TP.IdTipoPet = R.IdTipoPet
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ExericiosSQL/Ecommerce1.5/Ecommerce-1.5-DDL.sql
@@ -0,0 +1,53 @@
+/*
+
+		      DDL - LINGUAGEM DE DEFINIÇÃO DE DADOS
+
+*/
+
+
+CREATE DATABASE EcommerceManha;
+
+USE EcommerceManha;
+
+CREATE TABLE Lojas(
+	IdLoja INT PRIMARY KEY IDENTITY,
+	NomeLoja VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Categorias(
+	IdCategoria INT PRIMARY KEY IDENTITY,
+	NomeCategoria VARCHAR(200) NOT NULL,
+	IdLoja INT FOREIGN KEY REFERENCES Lojas(IdLoja)
+);
+
+CREATE TABLE SubCategorias(
+	IdSubCategorias INT PRIMARY KEY IDENTITY,
+	NomeSubCategorias VARCHAR(200) NOT NULL,
+	IdCategoria INT FOREIGN KEY REFERENCES Categorias(IdCategoria)
+
+);
+
+CREATE TABLE Produtos(
+	IdProduto INT PRIMARY KEY IDENTITY,
+	NomeProduto VARCHAR(200) NOT NULL,
+	Valor VARCHAR(100) NOT NULL,
+	IdSubCategorias INT FOREIGN KEY REFERENCES SubCategorias(IdSubCategorias)
+);
+
+CREATE TABLE PedidosProdutos(
+	IdPedido INT PRIMARY KEY IDENTITY,
+	IdProduto INT FOREIGN KEY REFERENCES Produtos(IdProduto)
+);
+
+CREATE TABLE Clientes(
+	IdCliente INT PRIMARY KEY IDENTITY,
+	NomeCliente VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Pedidos(
+	IdPedido INT PRIMARY KEY IDENTITY,
+	NumeroPedido VARCHAR(100) NOT NULL,
+	DataPedido DATE,
+	Status CHAR(1),
+	IdCliente INT FOREIGN KEY REFERENCES Clientes(IdCliente)
+);

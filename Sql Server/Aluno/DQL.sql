d98a47e40206388fc07a9b59cee465e4d83757a6
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-05 16:25:35 -0300

    Exercicio - Feito em aula(Teste)

---
+++ Sql Server/Aluno/DQL.sql
@@ -0,0 +1,11 @@
+-- DQL
+use Escola
+Select * From Aluno
+Select * From Materia
+Select * from Nota
+
+SELECT * FROM Nota
+
+SELECT sum (Resultado) as NotaSoma from Nota
+SELECT count (Resultado) as QuantidadeNotas from Nota
+SELECT avg (Resultado) as Media from Nota
